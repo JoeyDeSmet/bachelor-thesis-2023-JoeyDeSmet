9342ef913631dac213ee28d7f06bbbe1d8e7f67d
Joey De Smet <joey@de-smet.vlaanderen>
2023-06-06 19:41:22 +0200

    Added GridContainer component

---
+++ docs/.vuepress/components/GridContainer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// Quickly grid up some elements with this component in markdown.
+
+const props = defineProps({
+    rows: {
+        type: Number,
+        default: 1
+    },
+    cols: {
+        type: Number,
+        default: 1
+    },
+    gap: {
+        type: String,
+        default: '1rem'
+    }
+});
+</script>
+
+<template>
+    <div :style="`display: grid; grid-template-columns: repeat(${props.cols}, 1fr); grid-template-rows: repeat(${props.rows}, 1fr); gap: ${props.gap}`">
+        <slot></slot>
+    </div>
+</template>
