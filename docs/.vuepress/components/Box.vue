8b3e51d56e9595b8a1fe87ea7d85ae5d4bd9985d
Joey De Smet <joey@de-smet.vlaanderen>
2023-06-06 20:09:26 +0200

    Added Box component

---
+++ docs/.vuepress/components/Box.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="box">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    border: 1px solid #ccc;
+    border-radius: 1rem;
+
+    padding: 15px;
+}
+</style>
